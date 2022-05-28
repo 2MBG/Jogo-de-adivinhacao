40de0952bcb9dbbeffc60a77d666f764dc423037
2MBG <marcelomartins.rv1@gmail.com>
2022-05-28 18:33:06 -0300

    Jogo de Adivinhação

---
+++ Jogo de adivinhação.py
@@ -0,0 +1,19 @@
+from random import randint
+from time import sleep
+
+print('\n')
+print('-' * 52)
+print('\033[33;1mTENTE ADIVINHAR QUAL NÚMERO O COMPUTADOR IRÁ GERAR!\033[m')
+print('-' * 52)
+
+num1 = int(input('\n\033[37;1mDigite um número de 0 a 5: \033[m'))
+num2 = randint(0,5)  # Use uniform para gerar números de ponto flutuante
+
+print('\n\033[34;1mProcessando...\033[m')
+sleep(1)
+print(f'\n\033[35;1mCOMPUTADOR: {num2}\033[m')
+
+if num1 == num2:
+    print('\n\033[32;1mParabéns! Você acertou!\033[m\n')
+else:
+    print('\n\033[31;1mQue pena, você errou!\033[m\n')
