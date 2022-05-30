7386a3cdb67d29b719b8495fafdfbc442a6fe79c
Marcelo Martins <93449600+2MBG@users.noreply.github.com>
2022-05-30 17:24:37 -0300

    Update Jogo de adivinhação.py

--- Jogo de adivinhação.py
+++ Jogo de adivinhação.py
@@ -11,7 +11,7 @@ num2 = randint(0,5)
 
 print('\n\033[34;1mProcessando...\033[m')
 sleep(1)
-print(f'\n\033[35;1mCOMPUTADOR: {num2}\033[m')
+print(f'\n\033[35;1mCOMPUTADOR:\033[m {num2}')
 
 if num1 == num2:
     print('\n\033[32;1mParabéns! Você acertou!\033[m\n')
